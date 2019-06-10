50e16412332f8f1b0dcf7b617a2e072c1006079a
Candice Parker <lexus2424@gmail.com>
2019-06-10 16:10:51 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,10 +4,8 @@ class Appointment
   @@all = []
   def initialize(patient, doctor, date)
     @patient = patient
-    
+     @doctor = doctor
     @date = date
-    
-   
     @@all << self
   end
   
