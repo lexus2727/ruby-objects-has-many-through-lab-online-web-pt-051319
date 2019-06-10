15bbfa68983787ab4b944aac0493fadffa152091
Candice Parker <lexus2424@gmail.com>
2019-06-10 14:52:26 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,15 @@
+class Patient
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(doctor, date)
+    appointments = Appointment.new(doctor, date)
+end
